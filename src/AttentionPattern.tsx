7b76667b536a26081bb240b94eb7efc8af02ca9b
Vinay Hiremath <github@vinayh.com>
2024-01-31 19:01:28 +0000

    Added attention patterns using CircuitsVis, still do not fully render upon tab change but may rewrite relevant parts of CircuitsVis

--- src/AttentionPattern.tsx
+++ src/AttentionPattern.tsx
@@ -1,13 +1,11 @@
-import { useState, ReactNode } from "react"
 import { AttentionHeads } from "circuitsvis"
 import { AttnPatternRes } from "./App"
 import { Tabs } from "@mantine/core"
 
 export default function AttentionPattern({ attnPatternRes }: { attnPatternRes: AttnPatternRes | null }) {
-    const [activeLayer, setActiveLayer] = useState<ReactNode>()
     if (attnPatternRes != null) {
         return <>
-            <Tabs orientation="vertical" onChange={(idx) => setActiveLayer(<AttentionHeads attention={attnPatternRes.patterns[Number(idx)].pattern[0]} tokens={attnPatternRes.tokens} />)}>
+            <Tabs orientation="vertical" variant="pills" radius="md">
                 <Tabs.List>
                     {attnPatternRes.patterns.map((p) => (
                         <Tabs.Tab value={p.layer.toString()}>Layer {p.layer}</Tabs.Tab>
@@ -16,7 +14,7 @@ export default function AttentionPattern({ attnPatternRes }: { attnPatternRes: A
 
                 {attnPatternRes.patterns.map((p) => (
                     <Tabs.Panel value={p.layer.toString()} p={30}>
-                        {activeLayer}
+                        <AttentionHeads key={p.layer} attention={p.pattern[0]} tokens={attnPatternRes.tokens} />
                     </Tabs.Panel>
                 ))}
             </Tabs>
